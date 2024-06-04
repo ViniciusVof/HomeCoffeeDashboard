58ae8c4cfe94a0029f1d21a48d810cf46288bd3a
Vinicius Oliveira de Freitas <vini@MacBook-Air-de-Vinicius.local>
2024-06-03 22:04:51 -0300

    feat: Configurando react-router-dom

--- src/Loader.tsx
+++ src/Loader.tsx
@@ -1,5 +1,14 @@
-import { Home } from "pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import * as Pages from "pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Pages.Home />,
+  },
+]);
 
 export function Loader() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
